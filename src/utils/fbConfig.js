021f24c967a9ab5176ec2ba60210feedf78a9056
Miguel Perello garcia <canyavall@gmail.com>
2017-11-14 15:00:27 +0100

    Update fbConfig.js

--- src/utils/fbConfig.js
+++ src/utils/fbConfig.js
@@ -1,10 +1,10 @@
 export const fbConfig = {
-    apiKey: "AIzaSyCT_yqrqzZ47sNf6vryqROk7WLpPFd0TCo",
-    authDomain: "fir-test-ea70d.firebaseapp.com",
-    databaseURL: "https://fir-test-ea70d.firebaseio.com",
-    projectId: "fir-test-ea70d",
-    storageBucket: "fir-test-ea70d.appspot.com",
-    messagingSenderId: "825561625374"
+    apiKey: "**",
+    authDomain: "**",
+    databaseURL: "**",
+    projectId: "**",
+    storageBucket: "**",
+    messagingSenderId: "**"
 }
 
 export default { fbConfig }
