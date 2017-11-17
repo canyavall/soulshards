3de17669cd390cce44cf5caf99f4fed15adaecc7
Miguel Perello garcia <canyavall@gmail.com>
2017-11-17 17:03:30 +0100

    remove new ignored files

--- src/utils/fbConfig.js
+++
@@ -1,8 +0,0 @@
-export const fbConfig = {
-    apiKey: "AIzaSyCT_yqrqzZ47sNf6vryqROk7WLpPFd0TCo",
-    authDomain: "fir-test-ea70d.firebaseapp.com",
-    databaseURL: "https://fir-test-ea70d.firebaseio.com",
-    projectId: "fir-test-ea70d",
-    storageBucket: "fir-test-ea70d.appspot.com",
-    messagingSenderId: "825561625374"
-}
